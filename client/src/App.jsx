aa3441fb088948782e9a07ea8cfa3abe192a8ac6
Harsh kumar <2001kumarharsh@gmail.com>
2024-10-06 00:20:56 +0530

    Add Homepage routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
       </Routes>
